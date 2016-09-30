cf32a03c4c52740301e23616acef40d00c75efee
huanggang <huanggang@tninkpad>
2016-09-30 17:51:02 +0800

    t提交

--- Application/Manage/Controller/FileController.class.php
+++ Application/Manage/Controller/FileController.class.php
@@ -89,7 +89,10 @@ class FileController extends AdminController {
             if (!empty($_FILES)) {
                 $size = $this->filedoc->fileSize($_FILES,0);
                 if (!empty($size)) {
-                    $result=$this->filedoc->saveUploadNull('FILE_PUB_DOC','FIP_PUB_DOC');
+                    $res['file_type']='FILE_PUB_DOC';
+                    $res['ftp_type']='FIP_PUB_DOC';
+                    $res['mark']=0;
+                    $result=$this->filedoc->saveUploadNull($res);
                     $data['doc_upload_file_url']=$result[0];
                     $data['doc_upload_img_url']=$result[1];
                     $result = $this->filedoc->addFile($data);
@@ -98,6 +101,7 @@ class FileController extends AdminController {
                     } else {
                         $this->error($result['status'], U('File/addFile'));
                     }
+                    return true;
                 } else {
                     $this->error(C('DOCFILE.SZIE_TYPE'), U('File/addFile'));
                 }
@@ -173,6 +177,7 @@ class FileController extends AdminController {
                 }else {
                     $this->error($result['status'], U('File/saveFile',array('doc_id'=>$data['doc_id'])));
                 }
+                return true;
         } 
         $doc_id=I('doc_id');
         $result = $this->filedoc->saveFileDoc($doc_id);

fc2905b22c063adc334f513441ce3f11e72dd52e
lihaijian <goodgamehjli@gmail.com>
2016-10-08 09:47:54 +0800

    系统消息必填项校验

--- Public/Js/configManage.js
+++ Public/Js/configManage.js
@@ -1,8 +1,16 @@
 //新增动作
 $("#add-btn").on("click", function () {
 
-    //judgeNull();
-    if (!$(".error-message").hasClass("show")) {
+    if($("[name=config_key]").val() == "-100"){
+        $.dialog.alert("请选择参数类别！");
+        return false;
+    }else if($.trim($("[name=config_descripion]").val()) == "" ){
+        $.dialog.alert("请填写参数名称！");
+        return false;
+    }else if($.trim($("[name=config_value]").val()) == "" ){
+        $.dialog.alert("请填写参数值！");
+        return false;
+    }else {
         $.ajax({
             url: "/Manage/configManage/add",
             data: $('#add_config_info').serialize(),
@@ -23,9 +31,16 @@ $("#add-btn").on("click", function () {
 
 //编辑页面
 $("#edit-btn").on("click", function () {
-
-    //judgeNull();
-    if (!$(".error-message").hasClass("show")) {
+    if($("[name=config_key]").val() == "-100"){
+        $.dialog.alert("请选择参数类别！");
+        return false;
+    }else if($.trim($("[name=config_descripion]").val()) == "" ){
+        $.dialog.alert("请填写参数名称！");
+        return false;
+    }else if($.trim($("[name=config_value]").val()) == "" ){
+        $.dialog.alert("请填写参数值！");
+        return false;
+    }else {
         $.ajax({
             url: "/Manage/configManage/edit",
             data: $('#edit_config_info').serialize(),
@@ -43,6 +58,7 @@ $("#edit-btn").on("click", function () {
 
         });
     }
+
 });
 
 //修改状态（停用/发布）
@@ -69,7 +85,7 @@ function changeStatus(config_id, config_status) {
 //单条删除
 function singleDel(config_id) {
     $.dialog({
-        title: '提示信息', content: '确认删除该系统消息吗？', ok: function () {
+        title: '提示信息', content: '确认删除系统参数吗？', ok: function () {
             $.ajax({
                 url: "/Manage/configManage/delete/config_id/" + config_id,
                 dataType: "json",
@@ -95,3 +111,8 @@ function showPro(target, msg, loca) {
         location.href = loca;
     }, 1500);
 }
+
+// 必填项验证
+$("#edit-btn").on("click",function(){
+
+})

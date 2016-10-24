e0374021efb92fcccacc3e11b8917932a74a8096
lihaijian <goodgamehjli@gmail.com>
2016-10-24 18:05:32 +0800

    首页样式调整

--- Public/Js/frame.js
+++ Public/Js/frame.js
@@ -7,24 +7,32 @@ var frame = {
 	'menuInit': function(event) {
 		event=window||window.event;
 		var _this = this;
+		var $menuLi = $(".sys-menu > li");
 		var $oh2s = $('.sys-menu > li > h2');
 		var $oSeclevelUls = $('.sys-menu > li > ul');
 		var $oSeclevelLis = $oSeclevelUls.children('li');
 		$oh2s.click(function(e) {
+			$(this).addClass("current").parent().siblings().children("h2").removeClass("current");
 			e=window.event||event;
 			var $showCnt=$(this).next();
+			if($(this).children("a").hasClass("index-href")){
+				var $href = $(this).children("a").attr("href");
+				location.href = $href;
+			}
 			if($showCnt.is(":visible")){
 				$showCnt.slideUp(200);
+				$(this).removeClass("current");
 				$(this).children("i").last().attr("class","ico ico-arrow-collapse");
 			}else{
 				$showCnt.slideDown(200);
 				$(this).children("i").last().attr("class","ico ico-arrow-extend");
+
 			}
 		});
 		//二级菜单点击事件
 		$oSeclevelLis.click(function(ev) {
-			$oSeclevelLis.removeClass('current');
-			$(this).addClass('current').parent().parent().addClass('current').siblings().removeClass('current');
+			$(this).addClass('current');
+			$(this).parent().siblings().addClass('current')
 			//用链接文字更改'当前位置'
 			_this.setCurrentLocation($(this).find("span").text());
 		});
